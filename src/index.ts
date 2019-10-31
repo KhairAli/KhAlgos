43355b9c1b232a704c02dad9e60b20093e734b86
khairali <khair.naqvi@gmail.com>
2019-10-31 22:37:50 +0500

    basic project structure

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { Test } from './test';
+import { LinkList } from './link-list/link-list';
+
+export const test = new Test();
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'ALGO'
+  }
+};
